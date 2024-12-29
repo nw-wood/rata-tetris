ce47e68e5f85e96aaa3b2c97f773312316a399f7
nw-wood <nathanyelw@gmail.com>
2024-12-28 23:52:28 -0500

    ability to change a minos rotation, and additional start offset params for all mino types

--- src/minos.rs
+++ src/minos.rs
@@ -1,32 +1,79 @@
 use rand::Rng;
 
-const T_BLOCK: u8 = 0;
-const J_BLOCK: u8 = 1;
-const Z_BLOCK: u8 = 2;
-const O_BLOCK: u8 = 3;
-const S_BLOCK: u8 = 4;
-const L_BLOCK: u8 = 5;
-const I_BLOCK: u8 = 6;
-const MINO_TYPES: u8 = 7;
+pub const T_BLOCK: u8 = 0;
+pub const J_BLOCK: u8 = 1;
+pub const Z_BLOCK: u8 = 2;
+pub const O_BLOCK: u8 = 3;
+pub const S_BLOCK: u8 = 4;
+pub const L_BLOCK: u8 = 5;
+pub const I_BLOCK: u8 = 6;
+pub const MINO_TYPES: u8 = 7;
 
 type Rotation = Vec<Vec<u8>>;
+
+const ROT_LEFT: u8 = 0;
+const ROT_RIGHT: u8 = 1;
 
 #[derive(Clone)]
 pub struct Mino {
     rotations: Vec<Rotation>,
     pub selected_mino: u8,
-    current_rotation: usize,
+    pub current_rotation: usize,
+    pub start_offset: (i8, i8),
 }
 
 impl Mino {
-    pub fn get_rotation(&self) -> Rotation {
-        self.rotations[self.current_rotation].clone()
+
+    pub fn get_rotation(&self) -> &Rotation {
+        &self.rotations[self.current_rotation]
     }
+
+    pub fn next_rotation(&mut self, direction: u8) -> &Rotation {
+        match direction {
+            ROT_LEFT => {
+                if self.current_rotation <= 0 {
+                    return &self.rotations[self.rotations.len() - 1];
+                } else {
+                    return &self.rotations[self.current_rotation - 1];
+                }
+            },
+            ROT_RIGHT => {
+                if self.current_rotation >= self.rotations.len() - 1{
+                    return &self.rotations[0];
+                } else {
+                    return &self.rotations[self.current_rotation + 1];
+                }
+            },
+            _ => { return &self.rotations[self.current_rotation] },
+        }
+    }
+
+    pub fn rotate(&mut self, direction: u8) {
+        match direction {
+            ROT_LEFT => {
+                if self.current_rotation <= 0 {
+                    self.current_rotation = self.rotations.len() - 1;
+                } else {
+                    self.current_rotation -= 1;
+                }
+            },
+            ROT_RIGHT => {
+                if self.current_rotation >= self.rotations.len() - 1 {
+                    self.current_rotation = 0;
+                } else {
+                    self.current_rotation += 1;
+                }
+            },
+            _ => {},
+        }
+    }
+
     pub fn new() -> Self {
 
         let mut rng = rand::thread_rng();
         let selected_mino = rng.gen_range(0..MINO_TYPES);
         let mut rotations: Vec<Rotation> = vec![];
+        let mut start_offset: (i8, i8) = (0, 0);
 
         match selected_mino {
             T_BLOCK => {
@@ -52,6 +99,7 @@ impl Mino {
                         vec![0, 1, 0],
                     ],
                 ];
+                start_offset = (10, 0);
             },
             J_BLOCK => {
                 rotations = vec![
@@ -76,6 +124,7 @@ impl Mino {
                         vec![0, 1, 0],
                     ],
                 ];
+                start_offset = (10, 0);
             },
             Z_BLOCK => {
                 rotations = vec![
@@ -90,6 +139,7 @@ impl Mino {
                         vec![0, 0, 1],
                     ],
                 ];
+                start_offset = (10, 0);
             },
             O_BLOCK => {
                 rotations = vec![
@@ -98,6 +148,7 @@ impl Mino {
                         vec![1, 1],
                     ],
                 ];
+                start_offset = (10, 1);
             },
             S_BLOCK => {
                 rotations = vec![
@@ -112,6 +163,8 @@ impl Mino {
                         vec![0, 1, 0],
                     ],
                 ];
+                //was 9
+                start_offset = (10, 0);
             },
             L_BLOCK => {
                 rotations = vec![
@@ -136,6 +189,7 @@ impl Mino {
                         vec![0, 1, 1],
                     ],
                 ];
+                start_offset = (10, 0);
             },
             I_BLOCK => {
                 rotations = vec![
@@ -152,6 +206,7 @@ impl Mino {
                         vec![0, 0, 1, 0],
                     ],
                 ];
+                start_offset = (8, -1);
             },
             _ => {},
         }
@@ -160,6 +215,7 @@ impl Mino {
             rotations,
             selected_mino,
             current_rotation: 0,
+            start_offset,
         }
     }
 }
