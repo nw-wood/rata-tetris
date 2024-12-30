dc793f10c605ce112b44ef3983bf272da7d335c2
nw-wood <nathanyelw@gmail.com>
2024-12-30 01:26:47 -0500

    COLORSgit add src/ui.rs! minos in the board are all now colored, falling mino is colored - in addition to that the stats board has been completed and also matches the levels color palette

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,5 @@
 use crate::consts::*;
 
-use crossterm::style::style;
 use once_cell::sync::Lazy;
 
 use crate::game::Game;
@@ -14,7 +13,7 @@ use std::{
 
 use ratatui::{
     buffer::Buffer,
-    layout::Rect,
+    layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Style},
     text::{Line, Span},
     widgets::{Block, Paragraph, Widget},
@@ -122,46 +121,98 @@ impl Widget for &Game {
         let bg_color = Color::Indexed(BACKGROUND_COLOR);
 
         let block = Block::bordered();
+        //let test_block = Block::default().borders(Borders::ALL)
         let board_block = Block::bordered().style(Style::default().fg(Color::White).bg(bg_color));
         let block_no_border = Block::new();
 
         let board_style = Style::default().fg(bg_color).bg(bg_color);
         let screen_style = Style::default().fg(bg_color).bg(bg_color);
         let element_style = Style::default().fg(Color::White).bg(bg_color);
-        let cell_style = Style::default().fg(Color::White);
         
         draw_element(PRECALC_SCREEN, &elements[RECT_SCREEN], &block, &screen_style, buf);
         draw_element(CONTROLS_TEXT, &elements[RECT_CONTROLS], &block_no_border, &element_style, buf);
 
+        let palette = match self.current_level % 10 {
+            0 => PALETTE_BLURPLE,
+            1 => PALETTE_LIME,
+            2 => PALETTE_PINK,
+            3 => PALETTE_SWAMP,
+            4 => PALETTE_MELON,
+            5 => PALETTE_LAKE,
+            6 => PALETTE_FACTORY,
+            7 => PALETTE_MUAVE,
+            8 => PALETTE_NARU,
+            9 => PALETTE_CREAM,
+            _ => PALETTE_BLURPLE,
+        };
+
+        let t_block_style = Style::new().fg(Color::Indexed(palette[0])).bg(bg_color);
+        let j_block_style = Style::new().fg(Color::Indexed(palette[1])).bg(bg_color);
+        let z_block_style = Style::new().fg(Color::Indexed(palette[2])).bg(bg_color);
+        let o_block_style = Style::new().fg(Color::Indexed(palette[3])).bg(bg_color);
+        let s_block_style = Style::new().fg(Color::Indexed(palette[4])).bg(bg_color);
+        let l_block_style = Style::new().fg(Color::Indexed(palette[5])).bg(bg_color);
+        let i_block_style = Style::new().fg(Color::Indexed(palette[6])).bg(bg_color);
+
         match self.game_state {
             STATE_PLAYING => {
                 draw_element("", &elements[RECT_BOARD], &board_block, &board_style, buf);
+                draw_element(format!("{}{:03}     ", TEXT_LINES, self.line_count).as_str(), &elements[RECT_LINES], &block, &element_style, buf);
+                draw_element(format!("\n {}\n     {:06} \n\n {}\n     {:06} \n ", "TOP", self.top_score, "SCORE", self.current_score).as_str(), &elements[RECT_SCORES], &block, &element_style, buf);
+                
+                draw_element(format!("{}{:02}  ", TEXT_LEVEL, self.current_level).as_str(), &elements[RECT_LEVEL], &block, &element_style, buf);
+                
+                //meed to get fancy to draw additional minos to the screen now
                 draw_element(TEXT_STATS, &elements[RECT_STATS], &block, &element_style, buf);
-                draw_element(TEXT_LINES, &elements[RECT_LINES], &block, &element_style, buf);
-                draw_element(TEXT_SCORES, &elements[RECT_SCORES], &block, &element_style, buf);
+                //build a layout of rects from the rect_stats whole thing needs chopped up into a 2 x 7 with tweaked spacing
+                let stats_rect_tweaked = Rect::new(elements[RECT_STATS].x + 3, elements[RECT_STATS].y + 3, elements[RECT_STATS].width - 4 , elements[RECT_STATS].height - 1);
+                let stats_boxes = Layout::new(Direction::Vertical, Constraint::from_maxes([3, 3, 3, 3, 3, 3, 2])).split(stats_rect_tweaked);
+                stats_boxes.iter().enumerate().for_each(|(index, rect)| {
+                    let mino_id = index + 1;
+                    let mino_count = self.statistics[index];
+                    let mino_style= match mino_id as u8 {
+                        I_BLOCK => (TEXT_MINO_I, i_block_style),
+                        J_BLOCK => (TEXT_MINO_J, j_block_style),
+                        L_BLOCK => (TEXT_MINO_L, l_block_style),
+                        O_BLOCK => (TEXT_MINO_O, o_block_style),
+                        Z_BLOCK => (TEXT_MINO_Z, z_block_style),
+                        S_BLOCK => (TEXT_MINO_S, s_block_style),
+                        T_BLOCK => (TEXT_MINO_T, t_block_style),
+                        _ => ("", element_style),
+                    };
+                    draw_element(mino_style.0, rect, &block_no_border, &mino_style.1, buf);
+                    let number_display_box = Layout::new(Direction::Horizontal, Constraint::from_percentages([60, 40])).split(*rect)[1];
+                    draw_element(format!(" {mino_count:03}").as_str(), &number_display_box, &block_no_border, &element_style, buf);
+                });
                 draw_element(TEXT_NEXT, &elements[RECT_NEXT], &block, &element_style, buf);
-                draw_element(TEXT_LEVEL, &elements[RECT_LEVEL], &block, &element_style, buf);
 
-                let mino = &self.current_mino;
-                let mino_cells = mino.get_rotation();
-
-                //draw the board cells that have been filled
                 self.board_state.iter().enumerate().for_each(|(cell_y, row)| {
                     row.iter().enumerate().for_each(|(cell_x, value)| {
                         let board_rect = &elements[RECT_BOARD];
                         let cell_screen_position = (board_rect.x + (cell_x as u16 * 2), board_rect.y + cell_y as u16);
                         let cell_rect = Rect::new(cell_screen_position.0 + 1, cell_screen_position.1 + 1, 2, 1);
-                        let cell_empty_style = Style::default().fg(Color::DarkGray);
-                        let cell_full_style = Style::default().fg(Color::Blue);
+                        let cell_empty_style = Style::default().fg(bg_color);
+
                         if *value != 0 {
-                            Paragraph::new(BLOCK).style(cell_full_style).render(cell_rect, buf);
+                            let style = match *value {
+                                T_BLOCK => t_block_style,
+                                J_BLOCK => j_block_style,
+                                Z_BLOCK => z_block_style, 
+                                O_BLOCK => o_block_style,
+                                S_BLOCK => s_block_style,
+                                L_BLOCK => l_block_style,
+                                I_BLOCK => i_block_style,
+                                _ => t_block_style,
+                            };
+
+                            Paragraph::new(BLOCK).style(style).render(cell_rect, buf);
                         } else {
                             Paragraph::new(EMPTY).style(cell_empty_style).render(cell_rect, buf);
                         }
                     });
                 });
 
-                mino_cells.iter().enumerate().for_each(|(y, row)| {
+                self.current_mino.get_rotation().iter().enumerate().for_each(|(y, row)| {
                     row.iter().enumerate().for_each(|(x, value)| {
                         if *value != 0 {
                             let board_rect = &elements[RECT_BOARD];
@@ -170,23 +221,25 @@ impl Widget for &Game {
                             let cell_y_pos = y as i8 + cell_screen_position.1;
                             if cell_y_pos <= board_rect.y as i8 { return; }
                             let cell_rect = Rect::new(cell_x_pos as u16, cell_y_pos as u16, 2, 1);
-                            Paragraph::new(BLOCK).style(cell_style).render(cell_rect, buf);
+
+                            let style = match self.current_mino.selected_mino {
+                                T_BLOCK => t_block_style,
+                                J_BLOCK => j_block_style,
+                                Z_BLOCK => z_block_style, 
+                                O_BLOCK => o_block_style,
+                                S_BLOCK => s_block_style,
+                                L_BLOCK => l_block_style,
+                                I_BLOCK => i_block_style,
+                                _ => t_block_style,
+                            };
+                            Paragraph::new(BLOCK).style(style).render(cell_rect, buf);
                         }
                     });
                 });
             }
-            STATE_PAUSED => {
-                //draw the pause screen
-                draw_element(BIG_TEXT_PAUSED, &elements[RECT_BIG_TEXT], &block, &element_style, buf);
-            },
-            STATE_START_SCREEN => {
-                //draw the TETRIS screen
-                draw_element(BIG_TEXT_TETRIS, &elements[RECT_BIG_TEXT], &block, &element_style, buf);
-            }
-            STATE_GAME_OVER => {
-                //draw the game over screen
-                draw_element(GAME_OVER_TEXT, &elements[RECT_GAME_OVER_TEXT], &block, &element_style, buf);
-            },
+            STATE_PAUSED => draw_element(BIG_TEXT_PAUSED, &elements[RECT_BIG_TEXT], &block, &element_style, buf),
+            STATE_START_SCREEN => draw_element(BIG_TEXT_TETRIS, &elements[RECT_BIG_TEXT], &block, &element_style, buf),
+            STATE_GAME_OVER => draw_element(GAME_OVER_TEXT, &elements[RECT_GAME_OVER_TEXT], &block, &element_style, buf),
             _ => {}
         }
     }
@@ -252,7 +305,15 @@ fn build_element_rects(area: &Rect) -> Vec<Rect> {
 }
 
 fn draw_element(text: &str, rect: &Rect, block: &Block, style: &Style, buf: &mut Buffer) {
-    Paragraph::new(text)
+    //background fill fix... should no longer need to append spaces 🧐 ...works good!
+    let text_padded = text.lines().map(|line| {
+        let line_width = line.len();
+        let pad_needed = (rect.width as isize - line_width as isize).max(0) as usize;
+        let padding = " ".repeat(pad_needed);
+        format!("{}{}", line, padding)
+    }).collect::<Vec<String>>().join("\n");
+
+    Paragraph::new(text_padded)
         .block(block.clone())
         .style(*style)
         .render(*rect, buf);
